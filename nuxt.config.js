d9573752f3b38bb626a86668451dde44034314ed
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-06-04 16:47:59 +0530

    cleared deploy error

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,10 +13,10 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Exhaust Baba | Bike Exhaust and Silencer Trader based in delhi and trade across span india' },
       { name: 'format-detection', content: 'telephone=no' },
-      { name="og:title", content="Exhaust Baba | Bike Exhaust and Silencer Trader"},
-      {name="og:type" ,content="Company.business"},
-      {name="og:url", content="https://exhaustbaba.com/"},
-      {name="og:image" ,content="../static/eblogotext.png"}
+      { name: "og:title", content:"Exhaust Baba | Bike Exhaust and Silencer Trader"},
+      { name: "og:type" ,content:"Company.business"},
+      { name: "og:url", content:"https://exhaustbaba.com/"},
+      { name: "og:image" ,content:"../static/eblogotext.png"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/eblogo.svg' }
