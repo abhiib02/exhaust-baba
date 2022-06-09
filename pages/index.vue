170272ab973b9d02ea74919a7f51c81d2f85be95
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-06-09 18:59:20 +0530

    Added H1 Tag

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,10 @@
 <div class="">
   <section id="main" class="fullvh center-column">
     <div class="">
-      <img class="logo" src="../static/eblogotext.svg" alt="">
+      <img class="logo" src="../static/eblogo.svg" alt="">
+      
     </div>
+    <h1>Exhaust Baba</h1>
     <div class="desc ">
       For our products we use only the best materials and the most advanced technologies. Thanks to careful design work, we can offer the best exhaust systems specifically designed for all categories of motorcycles.
     </div>
