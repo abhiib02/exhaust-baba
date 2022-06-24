323b18f5a0e29f5fbb447a7281393ab2bd5e8d92
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2022-06-24 11:56:53 +0530

    added text logo

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
 <div class="">
   <section id="main" class="fullvh center-column">
     <div class="">
-      <img class="logo" src="../static/eblogo.svg" alt="">
+      <img class="logo" src="../static/eblogotext.svg" alt="">
       
     </div>
-    <h1>Exhaust Baba</h1>
+    <h1 class="hidden">Exhaust Baba</h1>
     <div class="desc ">
       For our products we use only the best materials and the most advanced technologies. Thanks to careful design work, we can offer the best exhaust systems specifically designed for all categories of motorcycles.
     </div>
@@ -28,5 +28,7 @@ export default {
 
 
 <style>
-
+.hidden{
+  color:#ffffff00;
+}
 </style>
